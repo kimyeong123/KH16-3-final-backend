99d7b79df8a17a5fbd212d86ce80eb22aa6aa821
kimyeongdo123 <dudeh17@naver.com>
2025-12-04 15:20:25 +0900

    테스트

---
+++ kh16Final3/src/main/java/com/kh/final3/Kh16Final3Application.java
@@ -0,0 +1,13 @@
+package com.kh.final3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kh16Final3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kh16Final3Application.class, args);
+	}
+
+}
---
+++ kh16Final3/src/test/java/com/kh/final3/Kh16Final3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.final3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kh16Final3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
