c9561cfea5a6a01d6ade11a183b329e8e26e45b3
leeminsu-ai <dlalstn85451569@gmail.com>
2025-12-09 09:39:03 +0900

    dma

--- kh16Final3/src/main/java/com/kh/final3/dto/ProductDto.java
+++ kh16Final3/src/main/java/com/kh/final3/dto/ProductDto.java
@@ -1,6 +1,6 @@
 package com.kh.final3.dto;
 
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,20 +9,20 @@ import lombok.NoArgsConstructor;
 
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class ProductDto {
-private int productNo;
-private int productSeller;
-private String productName;
-private String productDescription; //상세설명
-private int productStartPrice;//시작가
-private int productCurrentPrice;//현재가(초기값=시작가)
-private Integer productInstantPrice;   //즉시 구매가(Null허용함)
-private Integer productFinalPrice;//Null허용해야함
-private LocalDateTime productStartDate; //경매시작시간
-private LocalDateTime productEndDate; //경매 마감시간
-private String productStatus; //BIDDING(진행중), ENDED(낙찰), DELIVERING(배송중), COMPLETED(완료)
-private Integer productBuyerNo; //낙찰자 Fk (낙찰전엔 Null)
-private LocalDateTime productRegistrationAt;//상품등록한시간?
-
+	private Long productNo;
+    private Long sellerNo;
+    private String name;
+    private Long categoryCode;
+    private String description;
+    private Long startPrice; 
+    private Long finalPrice;
+    private Long instantPrice; 
+    private Timestamp startTime; 
+    private Timestamp endTime; 
+    private String status;
+    private Long buyerNo;        
+    private Timestamp registrationTime;
+    private Timestamp endedTime;
 
 
 }
